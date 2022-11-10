1e8f30b3f5f0f06491c26743ddde2cdfdb5da92b
Gregg Tavares <github@greggman.com>
2022-11-10 16:54:53 +0900

    use typedoc for docs

--- docs/scripts/prep.js
+++
@@ -1,5 +0,0 @@
-[...document.querySelectorAll('code')]
-   .filter(e => e.parentElement.nodeName === 'PRE')
-   .forEach(elem => elem.parentElement.classList.add('prettyprint'));
-
-document.querySelectorAll('p br').forEach(e => e.remove());
