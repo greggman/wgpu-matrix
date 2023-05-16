e923b13939c9e151d0a56a12e816c00a5ac73040
Buck Evan <buck.2019@gmail.com>
2023-05-15 21:10:23 -0500

    add mat[34].d.ts
    fixes #12 ?

---
+++ dist/2.x/mat4.d.ts
@@ -0,0 +1,7 @@
+import { ArrayLike } from './array-like';
+/**
+ * A JavaScript array with 16 values, a Float32Array with 16 values, or a Float64Array with 16 values.
+ * When created by the library will create the default type which is `Float32Array`
+ * but can be set by calling {@link mat4.setDefaultType}.
+ */
+export declare type Mat4 = ArrayLike;
