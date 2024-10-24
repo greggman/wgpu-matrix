f2cd07b9f237ef832c24bfda6b613c2fb20a3c88
Iwo Plaza <iwoplaza@gmail.com>
2024-10-24 20:57:20 +0200

    feat: a wider BaseArgType

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,19 @@
 /**
+ * A type wider than `number[]`, omitting any instance functions
+ * unused by the API, e.g., map, sort. This allows the math
+ * functions to operate on a wider range of array-like
+ * values.
+ */
+export interface MutableNumberArray {
+	readonly length: number;
+	[n: number]: number;
+}
+
+/**
  * The types you can pass to most functions that take an
  * array of numbers.
  */
-export type BaseArgType = Float32Array | Float64Array | number[];
+export type BaseArgType = Float32Array | Float64Array | MutableNumberArray;
 
 function wrapConstructor<T extends new(...args: any[]) => any>(
   OriginalConstructor: T,
